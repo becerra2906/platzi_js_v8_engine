753a9c5c9d4f9901466fdbc86ca27212b29b21bb
Alejandro <alejobecerra1995@gmail.com>
2020-12-31 12:58:06 -0500

    finished class 3 notes and created class 4 file

--- clase3.js
+++ clase3.js
@@ -1,2 +1,9 @@
 // que es V8 y como funciona como interprete;
 
+
+// los diferentes navegadores tenian diferentes motores de javascript
+
+//como v8 ha mejorado mucho la estabilidad de js, entonces el resto de navegadores han migrado a v8.
+
+// v8 nace para mejorar la usabilidad y el desempeño de google maps. 
+
---
+++ clase4.js
@@ -0,0 +1,2 @@
+// que pasa dentro de v8 cuando recibe el archivo y lo compila
+
